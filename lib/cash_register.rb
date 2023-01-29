b5543df406f61c25abe4c82d954cb529c587bb72
fusion407 <fusion407@gmail.com>
2023-01-28 20:55:06 -0600

    commit tree b91130f07cc5ddd5e4cdbe126412273db0322077

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,45 @@
+require "pry"
 
+class CashRegister
+    attr_accessor :discount, :total, :all_items, :title
+    def initialize(discount=0)
+        @discount = discount
+        @total = 0
+        @all_items = []
+        @all_items_price = []
+    end
+    def total
+        @total
+    end
+    def add_item(title, price, quantity=1)
+        @title = title
+        @price = price
+        quantity.times {@all_items.push(@title)}
+        quantity.times {@all_items_price.push(@price)}
+
+        @total = @total + price * quantity
+    end
+    def apply_discount
+        @total = @total - @discount * 10
+        if @discount == 0
+            return "There is no discount to apply."
+        end
+        "After the discount, the total comes to $#{@total}."
+    end
+    def items
+        @all_items
+    end
+    def void_last_transaction
+        @total = @total - @all_items_price.last 
+        @all_items.pop
+        @all_items_price.pop
+        if !@all_items
+            @total = 0.0
+        end
+    end
+end
+
+# cash_object = CashRegister.new()
+# puts cash_object.instance_variables
+# 
+# binding.pry
